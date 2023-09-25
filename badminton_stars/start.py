2c17e2093e1e6e00c80f224e215483c2c716a002
Universesboy <73209527+Universesboy@users.noreply.github.com>
2023-09-25 00:52:34 -0400

    Add files via upload

---
+++ badminton_stars/start.py
@@ -0,0 +1,12 @@
+def print_badminton_players():
+badminton_players = [
+"张雨萌", "王宇航", "李文雅", "陈明明",
+"赵佳琪", "刘伟杰", "杨欣媛", "韩军涛",
+"王丽娜", "李磊", "刘青青", "赵晓阳",
+"张倩倩", "王超", "杨晨", "陈婷婷",
+"周宇", "李娜", "马骁", "朱欣怡"
+]
+for player in badminton_players:
+print(player)
+
+
---
+++ badminton_stars/start.sh
@@ -0,0 +1,6 @@
+if [ "$1" == "test" ]; then
+echo "test"
+elseif 
+[ "$1" == "run" ]; then
+python start.py
+fi
