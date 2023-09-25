77b271ef857f831b03a5413bcd7a8e285186ddfe
Universesboy <73209527+Universesboy@users.noreply.github.com>
2023-09-25 01:06:55 -0400

    Update start.py

--- badminton_stars/start.py
+++ badminton_stars/start.py
@@ -6,7 +6,19 @@ badminton_players = [
 "张倩倩", "王超", "杨晨", "陈婷婷",
 "周宇", "李娜", "马骁", "朱欣怡"
 ]
-for player in badminton_players:
-print(player)
+
+badminton_players.sort()
+columns = 2
+
+players_per_column = len(badminton_players) 
+
+for i in range(columns):
+    start_index = i * players_per_column
+    end_index = start_index + players_per_column
+    column_players = badminton_players[start_index:end_index]
+
+    for player in column_players:
+        print(player.ljust(20), end=" ")
+    print()  
 
 
