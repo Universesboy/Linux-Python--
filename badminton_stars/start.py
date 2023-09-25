e33c2ce388c62e054f345f6f55f12946a2589289
Universesboy <73209527+Universesboy@users.noreply.github.com>
2023-09-25 14:51:01 -0400

    Update start.py

--- badminton_stars/start.py
+++ badminton_stars/start.py
@@ -1,18 +1,8 @@
-def print_badminton_players():
-badminton_players = [
+badminton_players=  [
 "张雨萌", "王宇航", "李文雅", "陈明明",
 "赵佳琪", "刘伟杰", "杨欣媛", "韩军涛",
 "王丽娜", "李磊", "刘青青", "赵晓阳",
 "张倩倩", "王超", "杨晨", "陈婷婷",
 "周宇", "李娜", "马骁", "朱欣怡"
 ]
-
-badminton_players.sort()
-columns = 2
-
-players_per_column = len(badminton_players) 
-
-print(badminton_players)
-    print()  
-
-
+print (badminton_players)
