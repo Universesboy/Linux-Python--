f7c36b98c3dda555128f39c2c7ae111600f70dc0
Universesboy <73209527+Universesboy@users.noreply.github.com>
2023-10-04 01:08:09 -0400

    Update scraping.py

--- assets/scraping.py
+++ assets/scraping.py
@@ -1,28 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
-
-page = requests.get('https://www.badmintonalberta.ca/page/11046/PLAYER-PROFILES-Alberta') # Getting page HTML through request
-soup = BeautifulSoup(page.content, 'html.parser') # Parsing content using beautifulsoup
-soup.find('table')
-soup.find_all('table')[1]
-soup.find('table', class_ = 'ruler')
-table = soup.find_all('table')[1]
-print(table)
-world_titles = table.find_all('th')
-world_titles
-world_table_titles = [title.text.strip() for title in world_titles]
-
-print(world_table_titles)
-import pandas as pd
-df = pd.DataFrame(columns = world_table_titles)
-
-df
-column_data = table.find_all('tr')
-for row in column_data[1:]:
-    row_data = row.find_all('td')
-    individual_row_data = [data.text.strip() for data in row_data]
-    
-    length = len(df)
-    df.loc[length] = individual_row_data
-df
-df.to_csv(r'/home/colin/Desktop/Python/assets/player.csv', index = False)
+url = 'https://www.badminton.ca/ranking/category.aspx?id=36611&category=1994&p=1&ps=100'
+page = requests.get(url) 
+soup = BeautifulSoup(page.text, 'html')
+# print(soup)
+table = soup.find('table', class_='ruler')
+# print(table)
+world_titles = soup.find_all('th')
+# print(world_titles)
+world_titles_stripped = [title.strip() for title in world_titles if title is not None]
+print(world_titles_stripped)
+# import pandas as pd
+# df = pd.DataFrame(columns = world_table_titles)
+# df
+# column_data = table.find_all('tr')
+# for row in column_data[2:]:
+#     row_data = row.find_all('td')
+#     individual_row_data = [data.text.strip() for data in row_data]
+#     cleaned_row_data = [item for item in individual_row_data if item.strip()]
+# #length = len(df)
+# #df.loc[length] = individual_row_data
+# 
+#     print (cleaned_row_data )
+# df
+# df.to_csv('', index = False)
